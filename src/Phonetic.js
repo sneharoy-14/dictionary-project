bcd9c699c059d5e7315ef1a5554f88f63942490c
sneharoy-14 <sneharoy3104@gmail.com>
2025-05-02 12:06:53 +0100

    Added the audio of the word

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,6 +1,15 @@
 import React from "react";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
-  return <div className="Phonetic">{props.phonetic.text}</div>;
+  return (
+    <div className="Phonetic">
+      {props.phonetic.text && <p>{props.phonetic.text}</p>}
+      {props.phonetic.audio && (
+        <ReactAudioPlayer src={props.phonetic.audio} controls />
+      )}
+    </div>
+  );
 }
+<ReactAudioPlayer src="my_audio_file.ogg" autoPlay controls />;
